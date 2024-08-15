3160bec040d455c15d8c7185239e1c63eae74c73
SR <r.nomena.sitraka@gmail.com>
2024-08-15 21:36:05 +0300

    refactoring

--- src/main/java/dev/nomenarav/cashcard/CashCardController.java
+++ src/main/java/dev/nomenarav/cashcard/CashCardController.java
@@ -25,9 +25,9 @@ public class CashCardController {
 
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId, Principal principal) {
-        Optional<CashCard> cashCardOptional = Optional.ofNullable(cashCardRepository.findByIdAndOwner(requestedId, principal.getName()));
-        if(cashCardOptional.isPresent()){
-            return ResponseEntity.ok(cashCardOptional.get());
+        CashCard cashCard = findCashCard(requestedId, principal);
+        if(cashCard != null){
+            return ResponseEntity.ok(cashCard);
         }else{
             return ResponseEntity.notFound().build();
         }
@@ -61,7 +61,7 @@ public class CashCardController {
 
     @PutMapping("/{requestedId}")
     private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate, Principal principal){
-        CashCard cashCard = cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
+        CashCard cashCard = findCashCard(requestedId, principal);
         if(cashCard != null){
             CashCard updatedCashCard = new CashCard(cashCard.id(), cashCardUpdate.amount(), principal.getName());
             cashCardRepository.save(updatedCashCard);
@@ -69,5 +69,9 @@ public class CashCardController {
         }
         return ResponseEntity.notFound().build();
 
+    }
+
+    private CashCard findCashCard(Long requestedId, Principal principal){
+        return cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
     }
 }
